64fa6337d8eb8696e8a7d5c11f6ae8354000e84b
Estated <50859250+estated90@users.noreply.github.com>
2020-04-16 09:34:35 +0200

    modification in main to instanciate there
    First step of polymorphism in the main function

--- Project02Eclipse/src/com/hemebiotech/main/RunProgram.java
+++ Project02Eclipse/src/com/hemebiotech/main/RunProgram.java
@@ -2,6 +2,9 @@ package com.hemebiotech.main;
 
 import com.hemebiotech.analytics.*;
 import com.hemebiotech.reader.IReader;
+import com.hemebiotech.reader.ReadDataFromFile;
+import com.hemebiotech.sort.CreateOrderData;
+import com.hemebiotech.writer.Writer;
 
 /**
  * name : main
@@ -22,10 +25,14 @@ public class RunProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 AnalyticsCounter createFileWithSymptoms = new AnalyticsCounter("Project02Eclipse\\Symptoms.txt");
+		 AnalyticsCounter createFileWithSymptoms = new AnalyticsCounter("Project02Eclipse\\Symptoms.txt",
+				 new ReadDataFromFile(),
+			new CreateOrderData(),
+			new Writer());
 		 System.out.println("The file located in " + createFileWithSymptoms.getFilePath() + " has been read.");
 		 System.out.println("A file result.out has been created at the root folder");
 				
 	}
-
+	
+	
 }
